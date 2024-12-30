c3dc81403319d339192e5ea8a0c4198b08149087
Rasel Hossen <raselhossen052@gmail.com>
2024-12-30 23:03:36 +0600

    Enhance Companies section: implement horizontal scrolling animation for logos using motion effects

--- components/sections/Companies.tsx
+++ components/sections/Companies.tsx
@@ -1,9 +1,11 @@
+'use client';
 import AcmeCorpLogo from '@/public/images/acme-corp-logo.svg';
 import EchoValleyLogo from '@/public/images/echo-valley-logo.svg';
 import QuantumLogo from '@/public/images/quantum-logo.svg';
 import PulseLogo from '@/public/images/pulse-logo.svg';
 import OutsideLogo from '@/public/images/outside-logo.svg';
 import CelestialLogo from '@/public/images/celestial-logo.svg';
+import { motion } from 'motion/react';
 
 export const companies = [
   {
@@ -40,13 +42,22 @@ export const Companies = () => {
           Empowering creators at leading companies
         </h2>
         <div className='-mx-4 mt-20 flex overflow-x-clip lg:-mx-8'>
-          <div className='flex flex-none gap-18 px-18 md:gap-36'>
-            {companies.map(({ logo: Logo, name }) => (
-              <div key={name}>
+          <motion.div
+            initial={{ x: 0 }}
+            animate={{ x: '-50%' }}
+            transition={{
+              repeat: Infinity,
+              ease: 'linear',
+              duration: 30,
+            }}
+            className='flex flex-none gap-18 px-9 md:gap-36 md:px-18'
+          >
+            {[...companies, ...companies].map(({ logo: Logo }, index) => (
+              <div key={index}>
                 <Logo className='h-8' />
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
